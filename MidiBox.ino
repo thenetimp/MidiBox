525b07f4794b9c1ecb49df44187def6f8de3c871
James Andrews <contact@jamesmandrews.com>
2014-05-11 11:23:11 +0900

    Added some basic commenting

--- MidiBox.ino
+++ MidiBox.ino
@@ -5,7 +5,11 @@
  */
  #include <MIDI.h>
 
+// Constant represents the Digital I/O pins of the arduino.
 const int buttons[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+
+// These arrays represents states of buttons and of what has been
+// sent to the remote MIDI device, one state for each button
 int buttonStatus[] = {LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW};
 int midiStatus[] = {LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW};
 int toggleStatus[] = {LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW};
@@ -16,12 +20,14 @@ long toggleTimer[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
  */
 void setup()
 {
-  // Set up all the pins
+  // Set up all the pins as an input
   for(int i=0; i < 10; i++)
   {
     pinMode(buttons[i], INPUT);
   }
 
+  // Tell MIDI to read all channels though we are not going to
+  // user it at the moment it is needed to initialize the library
   MIDI.begin(MIDI_CHANNEL_OMNI);
 }
 
@@ -33,11 +39,15 @@ void loop()
     // Get the current state of the button at btnIdx
     buttonStatus[btnIdx] = digitalRead(buttons[btnIdx]);
 
-    // If the button state goes high.
+    // If the button state goes HIGH toggle the midiStatus.
     if(buttonStatus[btnIdx] == HIGH && toggleStatus[btnIdx] == LOW  && toggleTimer[btnIdx] < (millis() - 400))
     {
+      // is used to keep us from sending a constant button push during the loop. 
       toggleStatus[btnIdx] = HIGH;
       toggleTimer[btnIdx] = millis();
+      
+      // If the MIDI status is LOW change it HIGH
+      // else the MIDI status is HIGH change it LOW
       if(midiStatus[btnIdx] == LOW)
       {
         MIDI.sendControlChange((btnIdx+1), 127, 1);
@@ -50,6 +60,8 @@ void loop()
       }
     } 
 
+    // If we're released the button then we can set the toggleStatus LOW
+    // to prepare for the next time we push the button.
     if(buttonStatus[btnIdx] == LOW)
     {
       toggleStatus[btnIdx] = LOW;
